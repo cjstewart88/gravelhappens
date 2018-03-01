26204be0c6f70ecdc6a52158ef52cd9ea8991d3a
Cody Stewart <cjstewart88@gmail.com>
2018-03-01 17:05:45 -0500

    added ride with gps iframe and images to route view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppRootComponent } from './components/app-root/app-root.component';
-import { GeorgiaMapComponent } from './components/georgia-map/georgia-map.component';
 import { RoutesComponent } from './components/routes/routes.component';
 import { RouteComponent } from './components/route/route.component';
 
 import { RoutesService } from './services/routes.service';
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
 
 @NgModule({
   declarations: [
     AppRootComponent,
     RoutesComponent,
     RouteComponent,
-    GeorgiaMapComponent
+    SafeUrlPipe
   ],
   imports: [
     BrowserModule,
--- src/app/services/routes.service.ts
+++ src/app/services/routes.service.ts
@@ -8,64 +8,18 @@ export class RoutesService {
     {
       id: 'the-dirty-sheets',
       name: 'The Dirty Sheets',
-      location: 'Chattahoochee Hills',
-      distance: 20,
-      shortDescription:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.`,
-        longDescription:
-        `<p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>`
+      location: 'Palmetto, GA',
+      distance: 19.9,
+      rideWithGpsId: 26950038,
+      pictures: ['1.JPG', '2.JPG', '3.JPG', '4.JPG']
     },
     {
       id: 'soggy-feet',
       name: 'Soggy Feet',
-      location: 'Juliette',
-      distance: 27,
-      shortDescription:
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.`,
-      longDescription:
-        `<p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>`
+      location: 'Juliette, GA',
+      distance: 27.8,
+      rideWithGpsId: 26914844,
+      pictures: ['1.JPG', '2.JPG', '3.JPG', '4.JPG', '5.JPG']
     }
   ];
 
